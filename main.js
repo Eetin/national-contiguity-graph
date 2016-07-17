131617d7d31fc5cbf2813f6699cee4348ffb8d9c
Eetin <p17dmitry@gmail.com>
2016-07-17 15:02:58 +0300

    fixed d3 script url

--- main.js
+++ main.js
@@ -60,43 +60,21 @@
 	    width = canvas.width,
 	    height = canvas.height;
 	
-	var simulation = d3.forceSimulation().force("charge", d3.forceManyBody().strength(-80)).force("link", d3.forceLink().id(function (d, i) {
+	var simulation = d3.forceSimulation().force("charge", d3.forceManyBody().strength(-90)).force("link", d3.forceLink().id(function (d, i) {
 	  return i;
-	}).distance(50).strength(1)).force('x', d3.forceX()).force('y', d3.forceY());
-	// .force('collide', d3.forceCollide(25))
-	// .force("center", d3.forceCenter(width / 2, height / 2))
+	}).distance(50)).force('x', d3.forceX()).force('y', d3.forceY());
 	
 	d3.json("https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json", function (error, graph) {
 	  if (error) throw error;
 	
 	  var nodes = graph.nodes;
-	  // const links = graph.links.map(d => ({ source: nodes[d.source], target: nodes[d.target] }))
 	  var links = graph.links;
 	
-	  // function forceBoundingBox(alpha) {
-	  //   for (var i = 0, n = nodes.length, node, k = alpha * 0.1; i < n; ++i) {
-	  //     node = nodes[i];
-	  //     if (node.x < 10) {
-	  //       node.x = 10
-	  //     } else if (node.x > width - 11) {
-	  //       node.x = width - 11
-	  //     }
-	  //     if (node.y < 10) {
-	  //       node.y = 10
-	  //     } else if (node.y > height - 11) {
-	  //       node.y = height - 11
-	  //     }
-	  //   }
-	  // }
-	
-	  simulation.nodes(nodes).on("tick", ticked);
-	
-	  simulation.force("link").links(links);
+	  simulation.nodes(nodes).links(links).force("link").on("tick", ticked);
 	
 	  d3.select(canvas).call(d3.drag().container(canvas).subject(dragsubject).on("start", dragstarted).on("drag", dragged).on("end", dragended));
 	
 	  function ticked() {
-	    // forceBoundingBox(.1);
 	
 	    context.clearRect(0, 0, width, height);
 	    context.save();
@@ -144,8 +122,8 @@
 	}
 	
 	function drawNode(d) {
-	  context.moveTo(d.x + 3, d.y);
-	  context.arc(d.x, d.y, 3, 0, 2 * Math.PI);
+	  context.moveTo(d.x + 10, d.y);
+	  context.arc(d.x, d.y, 10, 0, 2 * Math.PI);
 	}
 
 /***/ },
